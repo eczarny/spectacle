1a20af61db0e32f43a1dcfb3786bc7312f1fa96e
Eric Czarny <eczarny@gmail.com>
2011-11-15 19:51:31 -0500

    Spectacle doesn't need so many copies of the license.

--- SpectacleConstants.h
+++ SpectacleConstants.h
@@ -1,81 +1,59 @@
-// 
-// Copyright (c) 2011 Eric Czarny <eczarny@gmail.com>
-// 
-// Permission is hereby granted, free of charge, to any person obtaining a copy
-// of  this  software  and  associated documentation files (the "Software"), to
-// deal  in  the Software without restriction, including without limitation the
-// rights  to  use,  copy,  modify,  merge,  publish,  distribute,  sublicense,
-// and/or sell copies  of  the  Software,  and  to  permit  persons to whom the
-// Software is furnished to do so, subject to the following conditions:
-// 
-// The  above  copyright notice and this permission notice shall be included in
-// all copies or substantial portions of the Software.
-// 
-// THE  SOFTWARE  IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-// IMPLIED,  INCLUDING  BUT  NOT  LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-// FITNESS  FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-// AUTHORS  OR  COPYRIGHT  HOLDERS  BE  LIABLE  FOR ANY CLAIM, DAMAGES OR OTHER
-// LIABILITY,  WHETHER  IN  AN  ACTION  OF CONTRACT, TORT OR OTHERWISE, ARISING
-// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
-// IN THE SOFTWARE.
-// 
+#define SpectaclePreferencePaneName                   @"Spectacle"
+#define SpectacleHelperApplicationName                @"SpectacleHelper"
+#define SpectacleBundleIdentifier                     @"com.divisiblebyzero.Spectacle"
+#define SpectacleHelperBundleIdentifier               @"com.divisiblebyzero.SpectacleHelper"
 
-#define SpectaclePreferencePaneName                      @"Spectacle"
-#define SpectacleHelperApplicationName                   @"SpectacleHelper"
-#define SpectacleBundleIdentifier                        @"com.divisiblebyzero.Spectacle"
-#define SpectacleHelperBundleIdentifier                  @"com.divisiblebyzero.SpectacleHelper"
-
-#define SpectacleApplicationBundleExtension              @"app"
+#define SpectacleApplicationBundleExtension           @"app"
 
 #pragma mark -
 
-#define SpectacleNibName                                 @"Spectacle"
-#define SpectaclePreferencePaneNibName                   @"SpectaclePreferencePane"
+#define SpectacleNibName                              @"Spectacle"
+#define SpectaclePreferencePaneNibName                @"SpectaclePreferencePane"
 
 #pragma mark -
 
-#define SpectacleHelperControllerServiceName             @"SpectacleHelperControllerServiceName"
+#define SpectacleHelperControllerServiceName          @"SpectacleHelperControllerServiceName"
 
 #pragma mark -
 
-#define SpectacleHelperDidFinishLaunchingNotification    @"SpectacleHelperDidFinishLaunchingNotification"
-#define SpectacleHelperDidTerminateNotification          @"SpectacleHelperDidTerminateNotification"
-#define SpectacleHelperShouldTerminateNotification       @"SpectacleHelperShouldTerminateNotification"
+#define SpectacleHelperDidFinishLaunchingNotification @"SpectacleHelperDidFinishLaunchingNotification"
+#define SpectacleHelperDidTerminateNotification       @"SpectacleHelperDidTerminateNotification"
+#define SpectacleHelperShouldTerminateNotification    @"SpectacleHelperShouldTerminateNotification"
 
 #pragma mark -
 
-#define SpectacleHotKeyTranslationsPropertyListFile      @"SpecialHotKeyTranslations"
+#define SpectacleHotKeyTranslationsPropertyListFile   @"SpecialHotKeyTranslations"
 
-#define SpectacleHotKeyTranslationsKey                   @"SpecialHotKeyTranslations"
-#define SpectacleHotKeyGlyphTranslationsKey              @"SpecialHotKeyGlyphTranslations"
-
-#pragma mark -
-
-#define SpectacleHotKeyNamesPropertyListFile             @"HotKeyNames"
+#define SpectacleHotKeyTranslationsKey                @"SpecialHotKeyTranslations"
+#define SpectacleHotKeyGlyphTranslationsKey           @"SpecialHotKeyGlyphTranslations"
 
 #pragma mark -
 
-#define SpectacleWindowActionMoveToCenter                @"MoveToCenter"
-#define SpectacleWindowActionMoveToFullscreen            @"MoveToFullscreen"
-#define SpectacleWindowActionMoveToLeftHalf              @"MoveToLeftHalf"
-#define SpectacleWindowActionMoveToRightHalf             @"MoveToRightHalf"
-#define SpectacleWindowActionMoveToTopHalf               @"MoveToTopHalf"
-#define SpectacleWindowActionMoveToBottomHalf            @"MoveToBottomHalf"
-#define SpectacleWindowActionMoveToUpperLeft             @"MoveToUpperLeft"
-#define SpectacleWindowActionMoveToLowerLeft             @"MoveToLowerLeft"
-#define SpectacleWindowActionMoveToUpperRight            @"MoveToUpperRight"
-#define SpectacleWindowActionMoveToLowerRight            @"MoveToLowerRight"
-#define SpectacleWindowActionMoveToLeftDisplay           @"MoveToLeftDisplay"
-#define SpectacleWindowActionMoveToRightDisplay          @"MoveToRightDisplay"
-#define SpectacleWindowActionMoveToTopDisplay            @"MoveToTopDisplay"
-#define SpectacleWindowActionMoveToBottomDisplay         @"MoveToBottomDisplay"
-
-#define SpectacleWindowActionUndoLastMove                @"UndoLastMove"
-#define SpectacleWindowActionRedoLastMove                @"RedoLastMove"
+#define SpectacleHotKeyNamesPropertyListFile          @"HotKeyNames"
 
 #pragma mark -
 
-#define SpectacleSliderBackgroundImage                   @"SliderBackground"
-#define SpectacleSliderMaskImage                         @"SliderMask"
-#define SpectacleSliderHandleImage                       @"SliderHandle"
-#define SpectacleSliderHandlePressedImage                @"SliderHandlePressed"
+#define SpectacleWindowActionMoveToCenter             @"MoveToCenter"
+#define SpectacleWindowActionMoveToFullscreen         @"MoveToFullscreen"
+#define SpectacleWindowActionMoveToLeftHalf           @"MoveToLeftHalf"
+#define SpectacleWindowActionMoveToRightHalf          @"MoveToRightHalf"
+#define SpectacleWindowActionMoveToTopHalf            @"MoveToTopHalf"
+#define SpectacleWindowActionMoveToBottomHalf         @"MoveToBottomHalf"
+#define SpectacleWindowActionMoveToUpperLeft          @"MoveToUpperLeft"
+#define SpectacleWindowActionMoveToLowerLeft          @"MoveToLowerLeft"
+#define SpectacleWindowActionMoveToUpperRight         @"MoveToUpperRight"
+#define SpectacleWindowActionMoveToLowerRight         @"MoveToLowerRight"
+#define SpectacleWindowActionMoveToLeftDisplay        @"MoveToLeftDisplay"
+#define SpectacleWindowActionMoveToRightDisplay       @"MoveToRightDisplay"
+#define SpectacleWindowActionMoveToTopDisplay         @"MoveToTopDisplay"
+#define SpectacleWindowActionMoveToBottomDisplay      @"MoveToBottomDisplay"
+
+#define SpectacleWindowActionUndoLastMove             @"UndoLastMove"
+#define SpectacleWindowActionRedoLastMove             @"RedoLastMove"
+
+#pragma mark -
+
+#define SpectacleSliderBackgroundImage                @"SliderBackground"
+#define SpectacleSliderMaskImage                      @"SliderMask"
+#define SpectacleSliderHandleImage                    @"SliderHandle"
+#define SpectacleSliderHandlePressedImage             @"SliderHandlePressed"
