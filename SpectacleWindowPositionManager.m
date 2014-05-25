3a3a0ad2d42aa85a54d1361fbdde2b4cf24ba332
Alexander Courtis <acourtis@atlassian.com>
2014-05-25 14:14:18 +1000

    reinstate blocks on moving for undo/redo

--- SpectacleWindowPositionManager.m
+++ SpectacleWindowPositionManager.m
@@ -246,36 +246,40 @@
         return;
     }
     
-    [self moveWindowRect:windowRect frontMostWindowElement:frontMostWindowElement];
-    movedWindowRect = [self rectOfWindowWithAccessibilityElement: frontMostWindowElement];
-    
-    // did we move exactly into the desired location?
-    if (!CGRectEqualToRect(movedWindowRect, windowRect)) {
-        CGRect adjustedWindowRect = windowRect;
+    if ((action != SpectacleWindowActionUndo) && (action != SpectacleWindowActionRedo)) {
         
-        // reduce size to fit
-        while (movedWindowRect.size.width > windowRect.size.width || movedWindowRect.size.height > windowRect.size.height) {
-            if (movedWindowRect.size.width > windowRect.size.width) {
-                adjustedWindowRect.size.width -= 1;
-            }
-            if (movedWindowRect.size.height > windowRect.size.height) {
-                adjustedWindowRect.size.height -= 1;
+        // attempt the move 
+        [self moveWindowRect:windowRect frontMostWindowElement:frontMostWindowElement];
+        movedWindowRect = [self rectOfWindowWithAccessibilityElement: frontMostWindowElement];
+        
+        // did we move exactly into the desired location?
+        if (!CGRectEqualToRect(movedWindowRect, windowRect)) {
+            CGRect adjustedWindowRect = windowRect;
+            
+            // reduce size to fit
+            while (movedWindowRect.size.width > windowRect.size.width || movedWindowRect.size.height > windowRect.size.height) {
+                if (movedWindowRect.size.width > windowRect.size.width) {
+                    adjustedWindowRect.size.width -= 1;
+                }
+                if (movedWindowRect.size.height > windowRect.size.height) {
+                    adjustedWindowRect.size.height -= 1;
+                }
+                
+                // give up if we're trying to shrink to half the desired as this resizing is just not working
+                if (adjustedWindowRect.size.width < windowRect.size.width / 2.0f || adjustedWindowRect.size.height < windowRect.size.height / 2.0f) {
+                    break;
+                }
+                
+                [self moveWindowRect:adjustedWindowRect frontMostWindowElement:frontMostWindowElement];
+                movedWindowRect = [self rectOfWindowWithAccessibilityElement: frontMostWindowElement];
             }
             
-            // give up if we're trying to shrink to half the desired as this resizing is just not working
-            if (adjustedWindowRect.size.width < windowRect.size.width / 2.0f || adjustedWindowRect.size.height < windowRect.size.height / 2.0f) {
-                break;
-            }
+            // centre the resized window, taking into account any quantization adjustments
+            adjustedWindowRect.origin.x += floor((windowRect.size.width - movedWindowRect.size.width) / 2.0f);
+            adjustedWindowRect.origin.y += floor((windowRect.size.height - movedWindowRect.size.height) / 2.0f);
             
             [self moveWindowRect:adjustedWindowRect frontMostWindowElement:frontMostWindowElement];
-            movedWindowRect = [self rectOfWindowWithAccessibilityElement: frontMostWindowElement];
         }
-        
-        // centre the resized window, taking into account any quantization adjustments
-        adjustedWindowRect.origin.x += floor((windowRect.size.width - movedWindowRect.size.width) / 2.0f);
-        adjustedWindowRect.origin.y += floor((windowRect.size.height - movedWindowRect.size.height) / 2.0f);
-        
-        [self moveWindowRect:adjustedWindowRect frontMostWindowElement:frontMostWindowElement];
     }
 }
 
