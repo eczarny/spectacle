c2cf0cb079789633eb3e9af64f77bd9ef77a0602
shseo <ssh0702@gmail.com>
2015-08-17 00:01:31 +0900

    add SpectacleAlertcontroller class

---
+++ Spectacle/SpectacleAlertController.h
@@ -0,0 +1,18 @@
+//
+//  DivideAlertController.h
+//  Divide
+//
+//  Created by shseo on 2015. 6. 23..
+//
+//
+
+#import <Cocoa/Cocoa.h>
+
+@class DivideApplicationController;
+
+
+@interface DivideAlertController : NSWindowController
+
+@property (nonatomic) NSInteger windowType;
+
+@end
