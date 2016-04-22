eeedacf9b4413a6bd58fb257d16f307aa02ad426
Eric Czarny <eczarny@gmail.com>
2016-04-22 00:52:42 -0400

    Add the lower left window calculation.

---
+++ Spectacle/Resources/Window Position Calculations/SpectacleLowerLeftWindowCalculation.js
@@ -0,0 +1,8 @@
+windowPositionCalculationRegistry.registerWindowPositionCalculationWithAction(function (windowRect, visibleFrameOfScreen) {
+    var calculatedWindowRect = windowRect;
+    calculatedWindowRect.x = visibleFrameOfScreen.x;
+    calculatedWindowRect.y = visibleFrameOfScreen.y;
+    calculatedWindowRect.width = Math.floor(visibleFrameOfScreen.width / 2.0);
+    calculatedWindowRect.height = Math.floor(visibleFrameOfScreen.height / 2.0);
+    return calculatedWindowRect;
+}, "SpectacleWindowActionLowerLeft");
