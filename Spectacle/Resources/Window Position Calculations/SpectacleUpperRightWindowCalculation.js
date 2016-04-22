c91f9a74bce569c2554179b32c77e5f820fd5500
Eric Czarny <eczarny@gmail.com>
2016-04-22 00:54:08 -0400

    Add the upper right window calculation.

---
+++ Spectacle/Resources/Window Position Calculations/SpectacleUpperRightWindowCalculation.js
@@ -0,0 +1,8 @@
+windowPositionCalculationRegistry.registerWindowPositionCalculationWithAction(function (windowRect, visibleFrameOfScreen) {
+    var calculatedWindowRect = windowRect;
+    calculatedWindowRect.x = visibleFrameOfScreen.x + Math.floor(visibleFrameOfScreen.width / 2.0);
+    calculatedWindowRect.y = visibleFrameOfScreen.y + Math.floor(visibleFrameOfScreen.height / 2.0) + (visibleFrameOfScreen.height % 2.0);
+    calculatedWindowRect.width = Math.floor(visibleFrameOfScreen.width / 2.0);
+    calculatedWindowRect.height = Math.floor(visibleFrameOfScreen.height / 2.0);
+    return calculatedWindowRect;
+}, "SpectacleWindowActionUpperRight");
