3cdce0040904fbc3fefb56086eb00a326b8915f4
Eric Czarny <eczarny@gmail.com>
2016-05-03 13:19:41 -0400

    Resolve aggressive fallback behavior when moving windows between displays.

--- Spectacle/Resources/Window Position Calculations/SpectacleWindowCalculationHelpers.js
+++ Spectacle/Resources/Window Position Calculations/SpectacleWindowCalculationHelpers.js
@@ -12,8 +12,12 @@ var SpectacleCalculationHelpers = (function () {
         var centeredMidY = Math.abs(CGRectGetMidY(rect2) - CGRectGetMidY(rect1)) <= 1.0;
         return CGRectContainsRect(rect1, rect2) && centeredMidX && centeredMidY;
     };
+    var rectFitsWithinRect = function(rect1, rect2) {
+        return (rect1.width <= rect2.width) && (rect1.height <= rect2.height);
+    };
     return {
         copyRect: copyRect,
         rectCenteredWithinRect: rectCenteredWithinRect,
+        rectFitsWithinRect: rectFitsWithinRect,
     };
 })();
