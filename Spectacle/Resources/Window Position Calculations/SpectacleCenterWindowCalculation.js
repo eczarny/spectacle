7b25ba1f967e930afcaa0801afdcfc5af89af8ab
Worriorbg <worriorbg@gmail.com>
2017-01-23 23:36:08 +0200

    - Seems that rounding flooring the position sometimes results in 1 or 2 pixels spacing. Rounding them fixes *#700 Left/Right half and then Center leaves a couple of pixels between window and menubar*

--- Spectacle/Resources/Window Position Calculations/SpectacleCenterWindowCalculation.js
+++ Spectacle/Resources/Window Position Calculations/SpectacleCenterWindowCalculation.js
@@ -1,8 +1,8 @@
 var SpectacleCenterWindowCalculation = (function () {
     var centerWindowRectInVisibleFrameOfScreen = function(windowRect, visibleFrameOfScreen) {
         var calculatedWindowRect = SpectacleCalculationHelpers.copyRect(windowRect);
-        calculatedWindowRect.x = Math.floor(visibleFrameOfScreen.width / 2.0) - Math.floor(windowRect.width / 2.0) + visibleFrameOfScreen.x;
-        calculatedWindowRect.y = Math.floor(visibleFrameOfScreen.height / 2.0) - Math.floor(windowRect.height / 2.0) + visibleFrameOfScreen.y;
+        calculatedWindowRect.x = Math.round((visibleFrameOfScreen.width - windowRect.width) / 2.0) + visibleFrameOfScreen.x;
+        calculatedWindowRect.y = Math.round((visibleFrameOfScreen.height - windowRect.height) / 2.0) + visibleFrameOfScreen.y;
         return calculatedWindowRect;
     };
     return {
